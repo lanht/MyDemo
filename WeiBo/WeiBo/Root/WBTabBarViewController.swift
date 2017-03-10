a85c8635693a02100b271317eb9d3a9c687d58ec
lanht <2318203932@qq.com>
2017-03-10 18:24:58 +0800

    update

--- WeiBo/WeiBo/Root/WBTabBarViewController.swift
+++ WeiBo/WeiBo/Root/WBTabBarViewController.swift
@@ -9,27 +9,50 @@
 import UIKit
 
 class WBTabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpChildViewControllers()
+        setUpComposeBtn()
         
-        for item in tabBar.items! as [UITabBarItem] {
-            item.setTitleTextAttributes([NSFontAttributeName : UIFont.systemFont(ofSize: 30)], for:UIControlState.normal)
-            item.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.red], for:UIControlState.normal)
-            item.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.black], for:.normal)
-        }
     }
+    
+    @objc fileprivate func composeStatus() {
+        print("撰写微博")
+    }
+    
+    lazy var composeBtn: UIButton = {
+        let btn = UIButton.init();
+        btn.isUserInteractionEnabled = true
+        btn.setImage(UIImage.init(named:"tabbar_compose_icon_add"), for: .normal)
+        btn.setBackgroundImage(UIImage.init(named: "tabbar_compose_button"), for: .normal)
+        btn.setBackgroundImage(UIImage.init(named: "tabbar_compose_button_highlighted"), for: .highlighted)
+        return btn
+    }()
 }
 
 extension WBTabBarViewController {
     
-     func setUpChildViewControllers() {
+    fileprivate func setUpComposeBtn() {
+      
+        tabBar.addSubview(composeBtn)
+        
+        let count = CGFloat((viewControllers?.count)!)
+        let w = tabBar.bounds.width / count - 1
+        
+        composeBtn.frame = tabBar.bounds.insetBy(dx: 2 * w, dy: 0)
+        
+        composeBtn.addTarget(self, action: #selector(composeStatus), for: .touchUpInside)
+        
+    }
+    
+    fileprivate func setUpChildViewControllers() {
         
         let array = [
             ["clsName":"WBHomeViewController","title":"首页","imageName":"tabbar_home"],
             ["clsName":"WBMassageViewController","title":"消息","imageName":"tabbar_message_center"],
+            ["clsName":"UIViewController"],
             ["clsName":"WBDiscoverViewController","title":"发现","imageName":"tabbar_discover"],
             ["clsName":"WBUserCenterViewController","title":"我","imageName":"tabbar_profile"],
         ]
@@ -39,6 +62,12 @@ extension WBTabBarViewController {
             arrayM.append(controller(dict: dict))
         }
         viewControllers = arrayM;
+        
+        for item in tabBar.items! as [UITabBarItem] {
+            item.setTitleTextAttributes([NSFontAttributeName : UIFont.systemFont(ofSize: 30)], for:UIControlState.normal)
+            item.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.red], for:UIControlState.normal)
+            item.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.black], for:.normal)
+        }
     }
     
     private func controller(dict:[String : String]) -> UIViewController {
